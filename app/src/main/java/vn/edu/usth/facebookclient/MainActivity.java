111fef3042b4c2f1c983ed9bd38a8cdd8920dfea
dtkienn <dtkien12300@gmail.com>
2020-12-18 04:40:07 +0700

    created tab layout with interaction

--- app/src/main/java/vn/edu/usth/facebookclient/MainActivity.java
+++ app/src/main/java/vn/edu/usth/facebookclient/MainActivity.java
@@ -1,14 +1,31 @@
 package vn.edu.usth.facebookclient;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
 
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private int[] tabIcons = {R.drawable.home, R.drawable.notifi, R.drawable.menu};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), 1);
+        adapter.AddFragment(new FragmentFirst());
+        adapter.AddFragment(new FragmentSecond());
+        adapter.AddFragment(new FragmentThird());
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+        for (int i = 0; i < 3; i++){
+            tabLayout.getTabAt(i).setIcon(tabIcons[i]);
+        }
     }
 }
---
+++ app/src/main/java/vn/edu/usth/facebookclient/ViewPagerAdapter.java
@@ -0,0 +1,42 @@
+package vn.edu.usth.facebookclient;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    private final String[] title = {""};
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return title[0];
+    }
+
+    public void AddFragment (Fragment fragment){
+        fragmentList.add(fragment);
+    }
+}
+
