d478cc4e9cf830646e3b27b122c8135b774732a3
Ntrug1 <59216905+Ntrug1@users.noreply.github.com>
2021-01-18 16:27:02 +0700

    create search activity

--- app/src/main/java/vn/edu/usth/facebookclient/MainActivity.java
+++ app/src/main/java/vn/edu/usth/facebookclient/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
--- app/src/main/java/vn/edu/usth/facebookclient/SearchActivity.java
+++ app/src/main/java/vn/edu/usth/facebookclient/SearchActivity.java
@@ -2,13 +2,30 @@ package vn.edu.usth.facebookclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private ImageButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        button = (ImageButton) findViewById(R.id.back);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSearchActivity();
+            }
+        });
+    }
+
+    public void openSearchActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivities(new Intent[]{intent});
     }
 }
