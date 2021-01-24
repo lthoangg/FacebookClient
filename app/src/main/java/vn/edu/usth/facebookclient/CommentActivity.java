4c4740494a1243c49d3c8c667267bc8136ca28f7
Lưu Quý Nhân <nhanlq.bi9178@st.usth.edu.vn>
2021-01-25 05:28:59 +0700

    Finished CreateCommentActivity

--- app/src/main/java/vn/edu/usth/facebookclient/CommentActivity.java
+++ app/src/main/java/vn/edu/usth/facebookclient/CommentActivity.java
@@ -2,15 +2,17 @@ package vn.edu.usth.facebookclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import org.w3c.dom.Comment;
 
 public class CommentActivity extends AppCompatActivity {
 
@@ -23,7 +25,24 @@ public class CommentActivity extends AppCompatActivity {
     private LinearLayout comment_display;
     private LinearLayout comment_container;
     private TextView finish;
+    private ImageButton btn;
+    private EditText u_content;
+    private LinearLayout ln;
 
+    public static String[] add(int n, String[] arr, String x)
+    {
+        int i;
+
+        // create a new array of size n+1
+        String newarr[] = new String[n + 1];
+
+        for (i = 0; i < n; i++)
+            newarr[i] = arr[i];
+
+        newarr[n] = x;
+
+        return newarr;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +61,7 @@ public class CommentActivity extends AppCompatActivity {
             name.setText(username[i]);
             comment.setText(content[i]);
             comment_container.addView(comment_display);
-            Log.i("Comment", "Created");
+            Log.i("Comment", "Generated");
         }
 
         finish = (TextView) findViewById(R.id.return_post);
@@ -52,6 +71,23 @@ public class CommentActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        btn = (ImageButton) findViewById(R.id.comment_submit);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                u_content = (EditText) findViewById(R.id.input_commment);
+                Log.i("Comment", "Created new comment: " + u_content.getText().toString());
+                add(content.length, content, u_content.getText().toString());
+                add(username.length, content, u_content.getText().toString());
+
+                u_content.setText("");
+//                Intent reload = new Intent(getApplicationContext(), CommentActivity.class);
+//                startActivity(reload);
+            }
+
+        });
+
 
     }
 }
